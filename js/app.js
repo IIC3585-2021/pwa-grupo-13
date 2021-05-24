75e6c6af93bdca3da003cb91abdee2ca596ef8c4
Rodrigo Contreras <rodancoci@gmail.com>
2021-05-24 07:08:40 -0400

    Fix cache working, plus network timeout

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("/serviceWorker.js",{
-        scope: "/images"
+        scope: "/"
       })
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,10 @@
 const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
-  // "/",
-  // "/index.html",
-  // "/css/style.css",
-  // "/js/app.js",
+  "/",
+  "/favicon.ico",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
   "/images/icons/icon-512x512.png",
   "/images/coffee.jpg",
   "/images/eiffel.jpg",
@@ -15,20 +16,32 @@ self.addEventListener("install", installEvent => {
   console.log("Installing service worker")
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
-      console.log("caching assets");
       cache.addAll(assets);
+      console.log("assets cached");
     })
   )
 })
 
 self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.' + evt);
-  evt.respondWith(fromCache(evt.request));
+  console.log('The service worker is serving the asset: ' + evt.request.url);
+  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+    return fromCache(evt.request);
+  }));
 });
 
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+    var timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId);
+      fulfill(response);
+    }, reject);
+  });
+}
+
 function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
+  return caches.open(staticDevCoffee).then(function(cache) {
+    return cache.match(request).then(function(matching) {
       return matching || Promise.reject('no-match');
     });
   });
